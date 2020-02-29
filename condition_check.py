0755a0c875c3690b53dd4e6109752f30e5024965
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-29 11:14:42 +0600

    first commit

---
+++ condition_check.py
@@ -0,0 +1,12 @@
+a = 51
+b = 51
+if a>b:
+   print('a is greater than b')
+elif b>a:
+    print('b is greater than a')
+else:
+    print('a is equal to b')
+
+x = -50
+if x>0:
+    print('this is positive number')
